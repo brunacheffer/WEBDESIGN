14896713d56a0290445b7952ebfb5b57b242fe07
brunacheffer <124625467+brunacheffer@users.noreply.github.com>
2023-10-27 21:41:16 -0300

    Add files via upload

---
+++ arranjos.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    var zindex = 10;
+    
+    $("div.card").click(function(e){
+      e.preventDefault();
+  
+      var isShowing = false;
+  
+      if ($(this).hasClass("show")) {
+        isShowing = true
+      }
+  
+      if ($("div.cards").hasClass("showing")) {
+    
+        $("div.card.show")
+          .removeClass("show");
+  
+        if (isShowing) {
+        
+          $("div.cards")
+            .removeClass("showing");
+        } else {
+    
+          $(this)
+            .css({zIndex: zindex})
+            .addClass("show");
+  
+        }
+  
+        zindex++;
+  
+      } else {
+    
+        $("div.cards")
+          .addClass("showing");
+        $(this)
+          .css({zIndex:zindex})
+          .addClass("show");
+  
+        zindex++;
+      }
+      
+    });
+  });
